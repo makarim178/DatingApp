22612c8261b271aa6814933e26dbce8aec1f3cb0
makarim178 <MirAshiful.Karim@triosstudent.com>
2021-02-19 19:57:56 -0500

    End of Section 12 - Error Fixed

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -32,10 +32,9 @@ export class RegisterComponent implements OnInit {
       dateOfBirth: ['', Validators.required],
       city: ['', Validators.required],
       country: ['', Validators.required],
-      password: ['', Validators.required, 
-        Validators.minLength(4), 
-        Validators.maxLength(8)],
-      confirmPassword: ['', Validators.required, this.matchValues('password')]
+      password: ['', [Validators.required, 
+        Validators.minLength(4), Validators.maxLength(8)]],
+      confirmPassword: ['', [Validators.required, this.matchValues('password')]]
     })
   }
 
