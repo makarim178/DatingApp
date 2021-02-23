51e6d84a6c0777617782822007cfee295264854b
makarim178 <MirAshiful.Karim@triosstudent.com>
2021-02-23 18:54:20 -0500

    End of Section 14

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using API.Extensions;
 
@@ -28,9 +29,8 @@ namespace API.Entities
         public string Country { get; set; }
         public ICollection<Photo> Photos { get; set; }
 
-        // public int GetAge() {
-        //     return DateOfBirth.CalculateAge();
-        // }
+        public ICollection<UserLike> LikedByUsers { get; set; }
+        public ICollection<UserLike> LikedUsers { get; set; }
         
     }
 }
