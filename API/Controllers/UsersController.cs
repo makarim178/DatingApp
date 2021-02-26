bd5a76cb01f533d43223fe9ebd447643dd1300bc
makarim178 <MirAshiful.Karim@triosstudent.com>
2021-02-26 01:20:59 -0500

    End of Section 16

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -50,7 +50,6 @@ namespace API.Controllers
         }
 
         // api/users/3
-        // [Authorize]
         [HttpGet("{username}", Name="GetUser")]
         public async Task<ActionResult<MemberDto>> GetUser(string username)
         {
